61a074c443f3108f14bc45e049acac4c9ea5946a
ostapkob <ostapkob.gmail.com>
2021-06-11 02:29:56 +1000

    js p030

--- Python/020_Сумма_цифр_факториала.py
+++
@@ -1,5 +0,0 @@
-import math
-import MyFunction
-
-f = math.factorial(100)
-print(f,'sum:', MyFunction.sum_char(f))
--- Python/029_Различные_степени.py
+++
@@ -1,3 +0,0 @@
-m=101
-comb =dict.fromkeys([(a**b) for a in range(2, m) for b in range(2, m)])
-print(len(comb))
